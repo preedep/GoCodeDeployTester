f9257a688774061011b06e28a1ebf2faa0acd0b7
preedee ponchevin <preedee.ponchevin@gmail.com>
2016-10-09 14:29:36 +0700

    - update code

--- api.go
+++ api.go
@@ -1,11 +1,16 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
 
 func main() {
 	iris.Get("/hi", func(ctx *iris.Context) {
 		ctx.Write("Hello world %s", "iris")
-	})
 
+	})
+	fmt.Println("Start Service....")
 	iris.Listen(":8080")
 }
