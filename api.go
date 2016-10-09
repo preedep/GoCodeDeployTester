da14e42565bb9bea6185a1f735abc48a818f5cde
preedee ponchevin <preedee.ponchevin@gmail.com>
2016-10-09 16:32:18 +0700

    - update code

--- api.go
+++ api.go
@@ -9,6 +9,9 @@ import (
 func main() {
 	iris.Get("/hi", func(ctx *iris.Context) {
 		ctx.SetCookieKV("name", "iris")
+
+		ctx.SetHeader("Data", "1234567")
+
 		ctx.Write("Hello world %s", ctx.GetCookie("name"))
 	})
 	fmt.Println("Start Service....")
